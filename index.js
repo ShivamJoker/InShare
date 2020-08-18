ffb9a0fa40d59cc83eb6dc0cca3a20bc1b9cda28
Shivam <SINGHSHIVAMKR@GMAIL.COM>
2020-08-18 18:47:52 +0530

    fix the progress error

--- index.js
+++ index.js
@@ -1,28 +1,82 @@
-const dropZone = document.querySelector(".drop-zone")
-const fileInput = document.querySelector("#fileInput")
-const browseBtn = document.querySelector("#browseBtn")
+const dropZone = document.querySelector(".drop-zone");
+const fileInput = document.querySelector("#fileInput");
+const browseBtn = document.querySelector("#browseBtn");
 
-browseBtn.addEventListener('click', ()=>{
-    fileInput.click()
-})
+browseBtn.addEventListener("click", () => {
+  fileInput.click();
+});
 
-dropZone.addEventListener("drop", (e)=>{
-    e.preventDefault()
-    console.log("dropped", e.dataTransfer.files[0].name);
-    dropZone.classList.remove("dragged")
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  console.log("dropped", e.dataTransfer.files[0].name);
+  if (e.dataTransfer.files.length) {
+    fileInput.files = e.dataTransfer.files;
+    uploadFile();
+  }
+  dropZone.classList.remove("dragged");
+});
+dropZone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  dropZone.classList.add("dragged");
 
-})
-dropZone.addEventListener("dragover", (e)=>{
-    e.preventDefault()
-    dropZone.classList.add("dragged")
+  // console.log("dropping file");
+});
 
-    // console.log("dropping file");
-})
+dropZone.addEventListener("dragleave", (e) => {
+  dropZone.classList.remove("dragged");
 
+  console.log("drag ended");
+});
 
-dropZone.addEventListener("dragleave", (e)=>{
-    dropZone.classList.remove("dragged")
+// file input change and uploader
+fileInput.addEventListener("change", () => {
+  uploadFile();
+});
 
-    console.log("drag ended");
-})
+const uploadFile = () => {
+  console.log("file added uploading");
+  files = fileInput.files;
+  const formData = new FormData();
+  formData.append("myfile", files[0]);
 
+  // upload file
+  const xhr = new XMLHttpRequest();
+
+//   xhr.upload.onprogress = updateProgress;
+  xhr.addEventListener("load", transferComplete);
+  xhr.addEventListener("error", transferFailed);
+  xhr.addEventListener("abort", transferCanceled);
+
+  console.log(xhr);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      console.log(xhr.responseText);
+    }
+  };
+
+  xhr.open("POST", "http://localhost:3000/api/files");
+  xhr.send(formData);
+};
+
+// progress on transfers from the server to the client (downloads)
+function updateProgress(oEvent) {
+  if (oEvent.lengthComputable) {
+    var percentComplete = (oEvent.loaded / oEvent.total) * 100;
+    console.log(percentComplete);
+    // ...
+  } else {
+    // Unable to compute progress information since the total size is unknown
+  }
+}
+
+function transferComplete(evt) {
+  console.log("The transfer is complete.", evt);
+}
+
+function transferFailed(evt) {
+  console.log("An error occurred while transferring the file.");
+}
+
+function transferCanceled(evt) {
+  console.log("The transfer has been canceled by the user.");
+}
