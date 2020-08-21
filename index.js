8bd1283a9346adf475c7a0a807c82cb3ef024462
Shivam <singhshivamkr@gmail.com>
2020-08-21 09:33:35 +0530

    Added url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "http://localhost:3000";
+const baseURL = "https://innshare.herokuapp.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
